711474370bc990fedc382ce67b4311907c7597e2
Mika Tuupola <tuupola@appelsiini.net>
2009-02-04 15:19:57 +0200

    Use jQuery 1.3.1 and update year.

--- jquery.filestyle.js
+++ jquery.filestyle.js
@@ -1,15 +1,13 @@
 /*
  * Style File - jQuery plugin for styling file input elements
  *  
- * Copyright (c) 2007-2008 Mika Tuupola
+ * Copyright (c) 2007-2009 Mika Tuupola
  *
  * Licensed under the MIT license:
  *   http://www.opensource.org/licenses/mit-license.php
  *
  * Based on work by Shaun Inman
  *   http://www.shauninman.com/archive/2007/09/10/styling_file_inputs_with_css_and_the_dom
- *
- * Revision: $Id$
  *
  */
 
